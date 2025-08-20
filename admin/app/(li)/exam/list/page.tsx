33ce03db0aa88c9f1ce9981e0fb0b81e2c20d3eb
matsumoto-aska <matsumoto@aska-ltd.jp>
2025-08-20 09:05:13 +0900

    管理機能のレイアウトと試験一覧の見た目作成

--- admin/app/(li)/exam/list/page.tsx
+++ admin/app/(li)/exam/list/page.tsx
@@ -5,12 +5,71 @@ import LoginForm from "@/components/auth/login/LoginForm";
  */
 export default function LoginPage() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 px-4">
-            <center>
-                <h1 className="text-4xl font-extrabold text-gray-800 mb-8">
-                    Page:edit/list
-                </h1>
-            </center>
+        /* ヘッダー分だけ高さを調整（モバイル対応） */
+        <div className="h-[calc(100vh-116px)] lg:h-[calc(100vh-52px)] flex flex-col justify-start bg-base-200">
+
+            {/* 画面遷移 */}
+            <div className="flex justify-center h-16">
+                <div className="w-[90%] py-3">
+                    <h2 className="text-gray-600 text-xl font-bold text-left mb-4">試験管理</h2>
+                </div>
+            </div>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-[90%]">
+                    <h2 className="text-gray-600 text-3xl font-bold text-center mb-4">試験一覧</h2>
+                </div>
+            </div>
+
+            {/* 表 */}
+            <div className="flex justify-center h-[75%]">
+                <div className="w-[90%] p-3 bg-white shadow rounded">
+                    <div className="h-full overflow-y-auto">
+                        <table className="table table-zebra w-full">
+                            <thead className="sticky top-0 bg-white">
+                            <tr className="text-center text-gray-600">
+                                <th className="w-10">ID</th>
+                                <th>試験名</th>
+                                <th className="w-28">開始日</th>
+                                <th className="w-28">終了日</th>
+                                <th className="w-25">合格者数</th>
+                                <th className="w-52">操作</th>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white">
+                            {Array.from({ length: 50 }).map((_, i) => (
+                                <tr key={i} className="text-gray-600">
+                                    <td className="text-center">{i + 1}</td>
+                                    <td>試験名 {i + 1}</td>
+                                    <td className="text-center">2025/01/01</td>
+                                    <td className="text-center">2025/01/31</td>
+                                    <td className="text-center">31/31</td>
+                                    <td className="text-center">
+                                        <button className="btn btn-sm border-slate-500 bg-slate-600 hover:bg-slate-500 text-white mx-1">
+                                            変更
+                                        </button>
+                                        <button className="btn btn-sm border-slate-500 bg-slate-600 hover:bg-slate-500 text-white mx-1">
+                                            状況
+                                        </button>
+                                        <button className="btn btn-sm border-slate-500 bg-slate-600 hover:bg-slate-500 text-white mx-1">
+                                            削除
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="w-[90%] text-right py-3">
+                    <button className="btn border-slate-500 bg-slate-600 hover:bg-slate-500 text-white mx-1">
+                        新規登録
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- admin/app/(li)/layout.tsx
+++ admin/app/(li)/layout.tsx
@@ -1,7 +1,129 @@
+"use client";
+
+import { CircleStackIcon, UsersIcon } from '@heroicons/react/24/outline'
+import { DocumentTextIcon, ClipboardDocumentIcon } from '@heroicons/react/24/outline'
+import { UserPlusIcon, UserIcon } from '@heroicons/react/24/outline'
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline'
+import { useState } from "react";
+
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
+
+    // 開いているメニューの設定
+    const [openMenu, setOpenMenu] = useState(null);
+
+    // メニュー選択時の処理
+    const toggleMenu = (menu) => {
+        setOpenMenu(openMenu === menu ? null : menu);
+    };
+
+    // 選択中のメニューの色
+    const activeMenuClass = "bg-slate-600";
+
     return (
         <main>
-            {children}
+            <div className="drawer lg:drawer-open">
+                {/* トグルボタン (モバイル向け) */}
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+
+                {/* サイドメニュー */}
+                <div className="drawer-side bg-slate-800">
+
+                    {/* ヘッダー */}
+                    <div className="z-10 w-60">
+                        <header className="w-full h-13 flex items-center justify-center border-b border-sky-800 bg-sky-800 text-white">
+                            <h1 className="text-lg font-bold">PMark Examination</h1>
+                        </header>
+                    </div>
+
+                    {/* メニュー */}
+                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+
+                    {/* ヘッダー分だけ高さを調整（モバイル対応）*/}
+                    <div className="w-60 mt-[52px] lg:mt-0">
+                        <ul className="w-full menu p-0 m-0 text-white overflow-y-auto">
+                            <li>
+                                <button
+                                    onClick={() => toggleMenu("examination")}
+                                    className={`w-full leading-9 text-left flex items-center gap-2 hover:${activeMenuClass} ${
+                                        openMenu === "examination" ? activeMenuClass : ""
+                                    }`}
+                                >
+                                    <CircleStackIcon className="w-5 h-5" />
+                                    試験管理
+                                </button>
+                                {openMenu === "examination" && (
+                                    <ul className="p-0 m-0">
+                                        <li><a className="pl-6 bg-slate-700 hover:bg-slate-500 leading-9 text-sm">
+                                            <DocumentTextIcon className="w-5 h-5" />
+                                            試験問題
+                                        </a></li>
+                                        <li><a className="pl-6 bg-slate-700 hover:bg-slate-500 leading-9 text-sm">
+                                            <ClipboardDocumentIcon className="w-5 h-5" />
+                                            受験状況
+                                        </a></li>
+                                    </ul>
+                                )}
+                            </li>
+
+                            <li>
+                                <button
+                                    onClick={() => toggleMenu("employee")}
+                                    className={`w-full leading-9 text-left flex items-center gap-2 hover:${activeMenuClass} ${
+                                        openMenu === "employee" ? activeMenuClass : ""
+                                    }`}
+                                >
+                                    <UsersIcon className="w-5 h-5" />
+                                    社員管理
+                                </button>
+                                {openMenu === "employee" && (
+                                    <ul className="p-0 m-0">
+                                        <li><a className="pl-6 bg-slate-700 hover:bg-slate-500 leading-9 text-sm">
+                                            <UserPlusIcon className="w-5 h-5" />
+                                            社員登録
+                                        </a></li>
+                                        <li><a className="pl-6 bg-slate-700 hover:bg-slate-500 leading-9 text-sm">
+                                            <UserIcon className="w-5 h-5" />
+                                            社員変更
+                                        </a></li>
+                                    </ul>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/* メインコンテンツ */}
+                <div className="drawer-content">
+        
+                    <label htmlFor="my-drawer" className="h-8 btn btn-sm btn-primary drawer-button lg:hidden m-4 z-10">
+                        メニューを開く
+                    </label>
+            
+                    {/* ヘッダー */}
+                    <header className="flex justify-end h-13 grid grid-cols-3 items-center justify-end border-b border-sky-700 bg-sky-700 text-white">
+                        
+                        {/* 左側（なし） */}
+                        <div></div>
+
+                        {/* 中央（タイトル） */}
+                        <h1 className="text-2xl font-bold text-center">
+                            Web試験システム
+                        </h1>
+
+                        {/* 右側（ログアウトボタン） */}
+                        <div className="flex justify-end pr-3">
+                            <button className="btn btn-sm border-slate-500 bg-slate-600 hover:bg-slate-500 text-white">
+                                <ArrowRightStartOnRectangleIcon className="w-5 h-5" />
+                                ログアウト
+                            </button>
+                        </div>
+                    </header>
+
+                    <div>
+                        {children}
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
